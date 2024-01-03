748f18b3dd51a341613efb6b2ec4c14d41362b82
younghwangit <twofireontree213@gmail.com>
2024-01-03 09:34:41 +0900

    update

--- study/1주차자습(4).py
+++ study/1주차자습(4).py
@@ -60,10 +60,11 @@ bicycle2.stop()
 """
 
 class Animal:
-    name = "" #Attribute, 클래스 함수 정의
+    def __init__(self,name = ""):
+        self.name = name
 
     def move(self):
-        print("동물은 운동성이 있다.")
+        print(f"{self.name}은 동물이고, 운동성이 있다.")
 
 class Mammal(Animal): #클래스 상속, Animal에서 Mammal로 기본기능을 제공함.(name과 move가 상속됨)
     def feed(self):
@@ -74,6 +75,7 @@ a.name = "고양이" #name 메소드에 할당
 a.move()
 a.feed()
 
-b = Animal
+b = Animal()
 b.name = "이구아나"
 b.move()
+#b.feed()
